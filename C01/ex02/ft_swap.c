5e13a34b16c4593ad6e704e52c0e3ac61ae497ec
dlamark- <119970138+debsalbornoz@users.noreply.github.com>
2023-05-12 16:42:17 +0000

    ft_swap

--- C01/ex02/ft_swap.c
+++ C01/ex02/ft_swap.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 void ft_swap(int *a , int *b)
 {
     int c;
@@ -5,4 +7,16 @@ void ft_swap(int *a , int *b)
     c = *a;
     *a = *b;
     *b = c;
+}
+
+int main(void)
+{
+ int a;
+ int b;
+
+ a = 50;
+ b = 30;
+ printf("Before call the function: a = %d and b = %d \n" , a ,b);
+ ft_swap(&a,&b);
+ printf("After call the function: a = %d and b = %d " , a, b);
 }
