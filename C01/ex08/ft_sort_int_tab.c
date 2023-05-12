047cdf521d458fd65013047d9dc3d9dc225dacb3
dlamark- <119970138+debsalbornoz@users.noreply.github.com>
2023-05-12 20:24:13 +0000

    ft_sort_int_tab

--- C01/ex08/ft_sort_int_tab.c
+++ C01/ex08/ft_sort_int_tab.c
@@ -1,4 +1,4 @@
-#include <unistd.h>
+#include <stdio.h>
 
 void	ft_sort_int_tab(int *tab, int size)
 {
@@ -22,3 +22,17 @@ void	ft_sort_int_tab(int *tab, int size)
 		}
 		i++;
 	}
+}
+
+int main(void)
+{
+	int tab [] = { 2,5,4,1,0,3};
+
+	ft_sort_int_tab(tab,6);
+
+	for (int i = 0; i < 6 ; i++)
+	{
+	printf("%i" , tab[i]);
+	}
+	printf("\n");
+}
