18bb5348a4375a23ecbc77a46a8b17a3fa74d648
dlamark- <119970138+debsalbornoz@users.noreply.github.com>
2023-05-12 17:47:41 +0000

    ft_ultimat_div_mod

--- C01/ex04/ft_ultimate_div_mod.c
+++ C01/ex04/ft_ultimate_div_mod.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 void	ft_ultimate_div_mod(int *a, int *b)
 {
 	int	c;
@@ -5,4 +7,16 @@ void	ft_ultimate_div_mod(int *a, int *b)
 	c = *a;
 	*a = *a / *b;
 	*b = c % *b;
+}
+
+int main(void)
+{
+int a;
+int b;
+
+a = 10;
+b = 5;
+ printf("Before call the function: a = %d and b = %d \n", a, b);
+ ft_ultimate_div_mod(&a, &b);
+ printf("After call the function: a = %d and b = %d \n" , a,b);
 }
