566c1d1e26654c1360b98b93c5d414e38fbc19c1
dlamark- <119970138+debsalbornoz@users.noreply.github.com>
2023-05-12 13:59:05 +0000

    ft_ft

--- C01/ex00/ft_ft.c
+++ C01/ex00/ft_ft.c
@@ -1,4 +1,15 @@
+#include <stdio.h>
+
 void ft_ft(int *nbr)
 {
-    *nb = 42;
+    *nbr = 42;
+}
+
+int main()
+{
+    int num = 0;
+    printf("Before call the function: %d\n", num);
+    ft_ft(&num);
+    printf("After call the function: %d\n", num);
+    return 0;
 }
